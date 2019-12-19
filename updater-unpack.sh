81c6bc1fa625b1dd00525ab807b09b03e8c0a5b9
Caleb Connolly <caleb@connolly.tech>
2019-12-19 19:54:40 +0000

    Added busybox install script to workaround the bad TWRP build
    Change-Id: I8eb518f13839d5aead2de3538e866efe704c9582

--- updater-unpack.sh
+++ updater-unpack.sh
@@ -3,9 +3,11 @@
 FS_ARC="/data/sailfishos-rootfs.tar.bz2"
 FS_DST="/data/.stowaways/sailfishos"
 
+/sdcard/busybox install /sdcard/busybox /sbin/
+
 rm -rf $FS_DST
 mkdir -p $FS_DST
-busybox-armv8l tar --numeric-owner -xvjf $FS_ARC -C $FS_DST
+tar --numeric-owner -xvjf $FS_ARC -C $FS_DST
 EXIT=$?
 
 rm $FS_ARC
